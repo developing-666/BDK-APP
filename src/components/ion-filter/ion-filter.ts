9e46174767616efaa83ca6555057c011b65a6e8e
777 <zwq8299174@126.com>
2018-05-31 20:57:42 +0800

    1

--- src/components/ion-filter/ion-filter.ts
+++ src/components/ion-filter/ion-filter.ts
@@ -1,19 +1,21 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
-
-import FilterData from '../../providers/filter-bar-data'
+import FilterData from '../../providers/filter-bar-data';
 
 @Component({
-    selector: "ion-filter",
-    templateUrl: "ion-filter.html"
+    selector: 'ion-filter',
+    templateUrl: 'ion-filter.html'
 })
 export class IonFilterComponent implements OnInit {
+    @ViewChild('backdrop') backdrop: ElementRef;
     filterData: any = FilterData;
     filterOpts: any;
     layout: number = 1;
-    openPanel: Boolean = true;
-    showBack: Boolean = false;
+    openPanel: boolean = false;
+    showBack: boolean = false;
+    backIn: boolean = false;
     activeIndex: number = 0;
+
     constructor() {
         console.log(this.filterOpts);
     }
@@ -25,15 +27,31 @@ export class IonFilterComponent implements OnInit {
         console.log(this.filterOpts.options);
         this.getLayout();
     }
-    getLayout(){
+    getLayout() {
         let layout = 1;
         for (let item of this.filterOpts.options) {
-            if (item.options && item.options.length>0) {
+            if (item.options && item.options.length > 0) {
                 layout = 2;
             }
         }
         this.layout = layout;
         console.log(this.layout);
-        
     }
+    open(i) {
+        // this.activeIndex = i;
+        console.log(i);
+        this.showBack = true;
+        setTimeout(() => {
+            this.openPanel = true;
+            this.backIn = true;
+        }, 0);
+    }
+    close() {
+        this.openPanel = false;
+        this.backIn = false;
+        setTimeout(() => {
+            this.showBack = false;
+        }, 200);
+    }
+    toggle() {}
 }
