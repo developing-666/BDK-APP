da13da616ddf569436a59f5793c09554fc16bf9d
haoyinde <haoyd3@asiainfo.com>
2018-05-31 20:55:29 +0800

    1

---
+++ src/pages/settings/user-info/user-info.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the UserInfoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+	selector: 'page-user-info',
+	templateUrl: 'user-info.html',
+})
+export class UserInfoPage {
+
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad UserInfoPage');
+	}
+
+}
