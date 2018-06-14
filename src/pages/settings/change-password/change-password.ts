f12b8e5e1f839702750e2fddbcb5574fbf720bb0
haoyinde <haoyd3@asiainfo.com>
2018-06-14 09:30:25 +0800

    1

--- src/pages/settings/change-password/change-password.ts
+++ src/pages/settings/change-password/change-password.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+import { Validators } from '../../../providers/validators';
 
 /**
  * Generated class for the ChangePasswordPage page.
@@ -9,16 +12,46 @@ import { NavController, NavParams } from 'ionic-angular';
  */
 
 @Component({
-  selector: 'page-change-password',
-  templateUrl: 'change-password.html',
+    selector: 'page-change-password',
+    templateUrl: 'change-password.html'
 })
 export class ChangePasswordPage {
+    ngForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        private fb: FormBuilder
+    ) {
+        this.createForm();
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ChangePasswordPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ChangePasswordPage');
+    }
 
+    /** 
+     * 创建表单 
+     */    
+    createForm() {
+    //     this.ngForm = this.fb.group({
+    //     old: ['',[Validators.required,Validators.minLength(6)]], 
+    //     new: ['',[Validators.required,Validators.minLength(6)]],
+    //     affirm: ['',[Validators.required,Validators.minLength(6)]], 
+    //   },{updateOn: 'blur'});
+    this.ngForm = new FormGroup({
+        old:new FormControl('',[Validators.required,Validators.minLength(6)]),
+        new:new FormControl('',[Validators.required,Validators.minLength(6)]),
+        affirm:new FormControl('',[Validators.required,Validators.minLength(6)]),
+    });
+    }
+    get old() { return this.ngForm.get('old'); }
+    get new() { return this.ngForm.get('new'); }
+    get affirm() { return this.ngForm.get('affirm'); }
+
+    change() {
+        console.log(this.ngForm);
+        console.log('this.old',this.old);
+        
+    }
 }
