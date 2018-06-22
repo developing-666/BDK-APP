adbcfc1f36bd375cd734b0486818a4fce163b53b
haoyinde <haoyd3@asiainfo.com>
2018-06-22 19:50:05 +0800

    1

--- src/providers/app-api.ts
+++ src/providers/app-api.ts
@@ -6,15 +6,30 @@ import { HttpService } from './http-service';
 export class AppApi {
 	constructor(
 		private _http: HttpService
-	) { }
+    ) { }
+    /** 
+     * 登录 
+     */    
 	login(opts?: any): any {
 		return this._http.post('/login', opts);
-	}
+    }
+    /** 
+     * 获取验证码 
+     */    
 	getCode(phone?: any): any {
 		return this._http.get('/authenticationcode/sms/'+phone);
-	}
+    }
+    /** 
+     * 注册 
+     */    
 	signIn(opts: any): any {
 		return this._http.post('/register', opts);
 	}
+    /** 
+     * 验证手机号
+     */    
+    signInPhoneValid(opts?: any): any {
+		return this._http.post('/register/valid', opts);
+    }
 
 }
