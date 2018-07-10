49de6766786598880fac2db09fe15b648fb21f80
zwq8299174 <zwq8299174@126.com>
2018-07-11 01:27:45 +0800

    7*

---
+++ src/pipes/moment/moment.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import moment from 'moment';
+moment.locale('zh-cn');
+@Pipe({
+	name: 'moment',
+})
+export class MomentPipe implements PipeTransform {
+	transform(value: string, ...args) {
+		console.log(value);
+		console.log(args);
+		if (value) {
+			return moment(value).format(args[0]);
+		} else {
+			return '';
+		}
+	}
+}
