6c71ac18c9e6d03dcff8b81ab6d371ac61fe161d
haoyinde <haoyd3@asiainfo.com>
2018-07-26 01:39:05 +0800

    清除缓存

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -7,31 +7,34 @@ import { PhoneHideNumberPipe } from './phone-hide-number/phone-hide-number';
 import { MomentPipe } from './moment/moment';
 import { GetImgPipe } from './get-img/get-img';
 import { SortNamePipe } from './sort-name/sort-name';
+import { SizeTransformPipe } from './size-transform/size-transform';
 
 export { MapToIterable } from './map-to-iterable';
 export { OrderBy } from './order-by';
 export { TrustResourceUrl } from './bypass-trust-res-url';
 
 @NgModule({
-	exports: [
-		MapToIterable,
-		OrderBy,
-		TrustResourceUrl,
-		FollowStatusPipe,
-		MomentPipe,
-		GetImgPipe,
+    exports: [
+        MapToIterable,
+        OrderBy,
+        TrustResourceUrl,
+        FollowStatusPipe,
+        MomentPipe,
+        GetImgPipe,
         PhoneHideNumberPipe,
-    SortNamePipe
-	],
-	declarations: [
-		MapToIterable,
-		OrderBy,
-		TrustResourceUrl,
-		FollowStatusPipe,
-		MomentPipe,
-		GetImgPipe,
+        SortNamePipe,
+        SizeTransformPipe
+    ],
+    declarations: [
+        MapToIterable,
+        OrderBy,
+        TrustResourceUrl,
+        FollowStatusPipe,
+        MomentPipe,
+        GetImgPipe,
         PhoneHideNumberPipe,
-    SortNamePipe
-	]
+        SortNamePipe,
+        SizeTransformPipe
+    ]
 })
-export class PipesModule { }
+export class PipesModule {}
